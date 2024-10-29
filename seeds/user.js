838dc57dbeea42b68662f2777eb9e5db0dea2a19
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-10-29 12:27:44 +0800

    add user seed for dummy data

---
+++ seeds/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const User = require("../models/user.models");
+
+mongoose
+  .connect("mongodb://localhost:27017/crud")
+  .then(async () => {
+    console.log("Connected to Database");
+
+    await User.deleteMany({});
+
+    const user = new User({
+      name: "Syahrul",
+      password: "123",
+    });
+
+    await user.save();
+    console.log("User created");
+
+    mongoose.disconnect();
+  })
+  .catch((err) => {
+    console.log("Failed to connect to Database", err);
+  });
