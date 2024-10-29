a3141f00fa82403574ccaf5d413e44c2daf2de62
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-10-30 03:32:28 +0800

    add schema models swagger

---
+++ swagger/schema.swagger.js
@@ -0,0 +1,35 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User Auth:
+ *       type: object
+ *       required:
+ *         - name
+ *         - password
+ *       properties:
+ *         name:
+ *           type: string
+ *         password:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *         - year
+ *       properties:
+ *         title:
+ *           type: string
+ *         author:
+ *           type: string
+ *         year:
+ *           type: integer
+ *           format: int32
+ */
