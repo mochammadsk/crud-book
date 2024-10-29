0c7c42e67c7ce38f92b01f410eae71c5cf97f10f
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-10-30 01:36:03 +0800

    update for use jwt

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,11 @@
 module.exports = (app) => {
-  const user = require("../controllers/auth.controllers");
+  const auth = require("../controllers/auth.controllers");
   const router = require("express").Router();
 
   // Login
   router.post("/signin", async (req, res) => {
     try {
-      await user.signin(req, res);
+      await auth.signin(req, res);
     } catch (err) {
       res.status(400).json({
         message: err.message,
