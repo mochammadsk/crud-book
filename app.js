5dd913b38a0f0cd69efa615cc04a57de4479de14
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-10-29 12:27:07 +0800

    add configur cors

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const express = require("express");
+const cors = require("cors");
+const { AutoEncryptionLoggerLevel } = require("mongodb");
 
 const app = express();
+
+const corsOption = {
+  origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+  method: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+  credentials: false,
+};
+
+app.use(cors(corsOption));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-API-Key"
+  );
+  next();
+});
 
 module.exports = app;
