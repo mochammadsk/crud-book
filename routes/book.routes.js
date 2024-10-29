2d44bc5f6d7ce34cc0b0c0cf531c590528dd6008
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-10-30 01:36:18 +0800

    udate for use jwt

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,24 +1,25 @@
 module.exports = (app) => {
+  const auth = require("../middleware/auth.middleware");
   const book = require("../controllers/book.controllers");
   const router = require("express").Router();
 
   // Create book
-  router.post("/create", book.createBook);
+  router.post("/create", auth, book.createBook);
 
   // Get all books
-  router.get("/view", book.getAllBooks);
+  router.get("/view", auth, book.getAllBooks);
 
   // Get book by id
-  router.get("/view/:id", book.getBookById);
+  router.get("/view/:id", auth, book.getBookById);
 
   //  Update book by id
-  router.put("/update/:id", book.updateBook);
+  router.put("/update/:id", auth, book.updateBook);
 
   // Delete book by id
-  router.delete("/delete/:id", book.deletOneBook);
+  router.delete("/delete/:id", auth, book.deletOneBook);
 
   // Delete all books
-  router.delete("/delete", book.deleteAllBooks);
+  router.delete("/delete", auth, book.deleteAllBooks);
 
   app.use("/books", router);
 };
