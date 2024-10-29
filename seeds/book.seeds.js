f377d35b1363656ff3290d4344554a8211352ad3
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-10-29 22:55:49 +0800

    add book seeds

---
+++ seeds/book.seeds.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const Book = require("../models/book.models");
+
+const bookData = [
+  {
+    title: "To Kill a Mockingbird",
+    author: "Harper Lee",
+    year: "1960 ",
+  },
+  {
+    title: "The Catcher in the Rye",
+    author: "J.D. Salinger",
+    year: "1951",
+  },
+  {
+    title: "Pride and Prejudice",
+    author: "Jane Austen",
+    year: "1813",
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    year: "1925",
+  },
+  {
+    title: "Moby-Dick",
+    author: "Herman Melville",
+    year: "1851",
+  },
+];
+
+mongoose
+  .connect("mongodb://localhost:27017/crud")
+  .then(async () => {
+    console.log("Connected to Database");
+
+    await Book.deleteMany({});
+
+    await Book.insertMany(bookData);
+    console.log("Books created");
+
+    mongoose.disconnect();
+  })
+  .catch((err) => {
+    console.log("Failed to connect to Database", err);
+  });
