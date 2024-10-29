ba81d6ebe0b7c89707b7e176a9a3f552b1b548e0
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-10-29 20:12:02 +0800

    rename file

---
+++ routes/auth.routes.js
@@ -0,0 +1,17 @@
+module.exports = (app) => {
+  const user = require("../controllers/user.controllers");
+  const router = require("express").Router();
+
+  // Login
+  router.post("/signin", async (req, res) => {
+    try {
+      await user.signin(req, res);
+    } catch (err) {
+      res.status(400).json({
+        message: err.message,
+      });
+    }
+  });
+
+  app.use("/", router);
+};
--- routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-  const user = require("../controllers/user.controllers");
-  const router = require("express").Router();
-
-  // Login
-  router.post("/user/login");
-
-  app.use("/", router);
-};
