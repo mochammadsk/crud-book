0676597bca0db52e10f8c934fc9bbbfca056d7da
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-05 00:30:05 +0800

    fix routes for login

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,14 +1,13 @@
 import { Application, Router, Request, Response } from "express";
-import auth from "../middleware/auth.middleware";
 import { signin } from "../controllers/auth.controllers";
 
 export default (app: Application): void => {
   const router = Router();
 
   // Login
-  router.post("/signin", auth, async (req: Request, res: Response) => {
+  router.post("/signin", async (req: Request, res: Response) => {
     try {
-      signin(req, res);
+      await signin(req, res);
     } catch (err: any) {
       res.status(400).json({
         message: err.message,
