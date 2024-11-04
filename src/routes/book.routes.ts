ffe8e5b388ce9e6ef790f960647c29d2e086f1e9
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-05 06:39:04 +0800

    update routes

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,27 +1,27 @@
-import { Application, Router } from "express";
-import { auth } from "../middleware/auth.middleware";
-import book from "../controllers/book.controllers";
+import { Application, Router } from 'express';
+import { auth } from '../middleware/auth.middleware';
+import book from '../controllers/book.controllers';
 
 export default (app: Application): void => {
   const router = Router();
 
   // Create book
-  router.post("/create", auth, book.createBook);
+  router.post('/create', auth, book.createBook);
 
   // Get all books
-  router.get("/view", book.getAllBooks);
+  router.get('/', book.getAllBooks);
 
   // Get book by id
-  router.get("/view/:id", book.getBookById);
+  router.get('/:id', book.getBookById);
 
   //  Update book by id
-  router.put("/update/:id", auth, book.updateBook);
+  router.put('/update/:id', auth, book.updateBook);
 
   // Delete all books
-  router.delete("/delete", auth, book.deleteAllBooks);
+  router.delete('/delete', auth, book.deleteAllBooks);
 
   // Delete book by id
-  router.delete("/delete/:id", auth, book.deleteOneBook);
+  router.delete('/delete/:id', auth, book.deleteOneBook);
 
-  app.use("/books", router);
+  app.use('/books', router);
 };
