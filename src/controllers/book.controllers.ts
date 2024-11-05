2b8076d85ebe897d70b12c727a15595f6d05b8fb
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-05 13:34:37 +0800

    fix bug from unitest

--- src/controllers/book.controllers.ts
+++ src/controllers/book.controllers.ts
@@ -7,28 +7,20 @@ export const createBook = async (
   res: Response
 ): Promise<void> => {
   try {
-    const { title, author, year } = req.body;
-
     // Check if book already exists
-    const existingBook = await Book.findOne({ title });
+    const existingBook = await Book.findOne({ title: req.body.title });
     if (existingBook) {
       res.status(400).send({ message: 'Book already exists!' });
       return;
     }
-
     // Create new book
-    const newBook = new Book({
-      title,
-      author,
-      year,
-    });
-
+    const newBook = new Book(req.body);
     await newBook.save();
     res
       .status(200)
       .send({ message: 'Book successfully added!', book: newBook });
   } catch (error) {
-    res.status(400).send({ message: 'error', error: error });
+    res.status(500).send({ message: 'error', error: error });
   }
 };
 
@@ -57,10 +49,14 @@ export const getBookById = async (
 ): Promise<void> => {
   try {
     const book = await Book.findById(req.params.id);
+    if (!book) {
+      res.status(404).send({ message: 'Book not found!' });
+      return;
+    }
 
     res.status(200).send({ message: 'Book found!', book });
   } catch (error) {
-    res.status(404).send({ message: 'error', error: error });
+    res.status(500).send({ message: 'error', error: error });
   }
 };
 
