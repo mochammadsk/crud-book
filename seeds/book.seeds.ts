ace5210d2d8a3d10cfc1039f940b360d12a16e61
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-11-05 15:35:19 +0800

    just update to ESmodule

--- seeds/book.seeds.ts
+++ seeds/book.seeds.ts
@@ -1,46 +1,46 @@
-const mongoose = require("mongoose");
-const Book = require("../models/book.models");
+import mongoose from 'mongoose';
+import Book from '../src/models/book.models';
 
 const bookData = [
   {
-    title: "To Kill a Mockingbird",
-    author: "Harper Lee",
-    year: "1960 ",
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    year: '1960 ',
   },
   {
-    title: "The Catcher in the Rye",
-    author: "J.D. Salinger",
-    year: "1951",
+    title: 'The Catcher in the Rye',
+    author: 'J.D. Salinger',
+    year: '1951',
   },
   {
-    title: "Pride and Prejudice",
-    author: "Jane Austen",
-    year: "1813",
+    title: 'Pride and Prejudice',
+    author: 'Jane Austen',
+    year: '1813',
   },
   {
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    year: "1925",
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    year: '1925',
   },
   {
-    title: "Moby-Dick",
-    author: "Herman Melville",
-    year: "1851",
+    title: 'Moby-Dick',
+    author: 'Herman Melville',
+    year: '1851',
   },
 ];
 
 mongoose
-  .connect("mongodb://localhost:27017/crud")
+  .connect('mongodb://localhost:27017/crud')
   .then(async () => {
-    console.log("Connected to Database");
+    console.log('Connected to Database');
 
     await Book.deleteMany({});
 
     await Book.insertMany(bookData);
-    console.log("Books created");
+    console.log('Books created');
 
     mongoose.disconnect();
   })
   .catch((err) => {
-    console.log("Failed to connect to Database", err);
+    console.log('Failed to connect to Database', err);
   });
--- seeds/user.seeds.ts
+++ seeds/user.seeds.ts
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
-const User = require("../models/auth.models");
+import mongoose from 'mongoose';
+import User from '../src/models/user.models';
 
 mongoose
-  .connect("mongodb://localhost:27017/crud")
+  .connect('mongodb://localhost:27017/crud')
   .then(async () => {
-    console.log("Connected to Database");
+    console.log('Connected to Database');
 
     await User.deleteMany({});
 
     const user = new User({
-      name: "Syahrul",
-      password: "123",
+      name: 'Syahrul',
+      password: '123',
     });
 
     await user.save();
-    console.log("User created");
+    console.log('User created');
 
     mongoose.disconnect();
   })
   .catch((err) => {
-    console.log("Failed to connect to Database", err);
+    console.log('Failed to connect to Database', err);
   });
